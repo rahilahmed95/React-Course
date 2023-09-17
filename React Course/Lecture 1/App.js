99efb1b74887b568832785f60c20cd567162debc
Rahil Ahmed <136297427+rahilahmed95@users.noreply.github.com>
2023-09-17 22:46:28 +0530

    Lecture 1 Uploaded with Assignment's Solution

---
+++ React Course/Lecture 1/App.js
@@ -0,0 +1,27 @@
+const heading1 = React.createElement(
+    "h1",
+    {
+        id: "heading1",
+        className: "heading1",
+    },
+    "Heading 1"
+);
+
+const heading2 = React.createElement(
+    "h2",
+    {
+        id: "heading2",
+        className: "heading2",
+    },
+    "Heading 2"
+);
+
+const container = React.createElement(
+    "div",
+    { id: "container", className: "container" },
+    [heading1, heading2]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
