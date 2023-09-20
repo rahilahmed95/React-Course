e72dd770ffc69ce27adc14ae98cc41c9f9127941
Rahil Ahmed <136297427+rahilahmed95@users.noreply.github.com>
2023-09-20 09:56:00 +0530

    Add files via upload

---
+++ React Course/Lecture 5/App.js
@@ -0,0 +1,602 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+  return (
+    <div className="navbar">
+      <a href="/">
+        <img
+          src="https://th.bing.com/th/id/OIP.Teo94KLctTWmO-B8uj8jTwHaHO?pid=ImgDet&rs=1"
+          alt="logo"
+        />
+      </a>
+      <ol className="navlist">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ol>
+    </div>
+  );
+};
+
+// Config driven UI
+const restrauntList = [
+  {
+    info: {
+      id: "392643",
+      name: "Chaayos Chai+Snacks=Relax",
+      cloudinaryImageId: "2beebd3688e63e027ee8a11c1c8a72c8",
+      locality: "Little World Mall",
+      areaName: "Kharghar",
+      costForTwo: "₹250 for two",
+      cuisines: [
+        "Bakery",
+        "Beverages",
+        "Chaat",
+        "Desserts",
+        "Home Food",
+        "Italian",
+        "Maharashtrian",
+        "Snacks",
+        "Street Food",
+        "Sweets",
+      ],
+      avgRating: 4.5,
+      feeDetails: {
+        restaurantId: "392643",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 5300,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 5300,
+      },
+      parentId: "281782",
+      avgRatingString: "4.5",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 27,
+        lastMileTravel: 5,
+        serviceability: "SERVICEABLE",
+        slaString: "27 mins",
+        lastMileTravelString: "5.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-09-20 23:00:00",
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: "guiltfree/GF_Logo_android_3x",
+            shortDescription: "options available",
+            fontColor: "#7E808C",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "",
+                  fontColor: "#7E808C",
+                  iconId: "guiltfree/GF_Logo_android_3x",
+                  shortDescription: "options available",
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/chaayos-chai-snacks-relax-little-world-mall-kharghar-mumbai-392643",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "415194",
+      name: "Burger King",
+      cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+      locality: "Kharghar -ltile world",
+      areaName: "Little world Mall",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American"],
+      avgRating: 4.2,
+      feeDetails: {
+        restaurantId: "415194",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 5300,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 5300,
+      },
+      parentId: "166",
+      avgRatingString: "4.2",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 29,
+        lastMileTravel: 5,
+        serviceability: "SERVICEABLE",
+        slaString: "29 mins",
+        lastMileTravelString: "5.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-09-21 03:45:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/burger-king-kharghar-ltile-world-little-world-mall-mumbai-415194",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "351396",
+      name: "McDonald's",
+      cloudinaryImageId: "ee5f8e06b300efc07c9fe3f4df40dfc4",
+      locality: "Sahyadri Apartments",
+      areaName: "Kalamboli",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Beverages", "Cafe", "Desserts"],
+      avgRating: 4.3,
+      feeDetails: {
+        restaurantId: "351396",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 3500,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 3500,
+      },
+      parentId: "630",
+      avgRatingString: "4.3",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 22,
+        lastMileTravel: 1.7,
+        serviceability: "SERVICEABLE",
+        slaString: "22 mins",
+        lastMileTravelString: "1.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-09-20 23:45:00",
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: "guiltfree/GF_Logo_android_3x",
+            shortDescription: "options available",
+            fontColor: "#7E808C",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "",
+                  fontColor: "#7E808C",
+                  iconId: "guiltfree/GF_Logo_android_3x",
+                  shortDescription: "options available",
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "10% OFF",
+        subHeader: "ABOVE ₹999",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/mcdonalds-sahyadri-apartments-kalamboli-mumbai-351396",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "434954",
+      name: "Samosa Singh",
+      cloudinaryImageId: "77baefd8a5e319c828b4d7dff7260644",
+      locality: "Raj Palace",
+      areaName: "Kharghar",
+      costForTwo: "₹150 for two",
+      cuisines: ["North Indian", "Indian", "Snacks", "Street Food"],
+      avgRating: 3.9,
+      veg: true,
+      feeDetails: {
+        restaurantId: "434954",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 5300,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 5300,
+      },
+      parentId: "5639",
+      avgRatingString: "3.9",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 27,
+        lastMileTravel: 5,
+        serviceability: "SERVICEABLE",
+        slaString: "27 mins",
+        lastMileTravelString: "5.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-09-20 09:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹100",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/samosa-singh-raj-palace-kharghar-mumbai-434954",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "432796",
+      name: "Paratha Box - Desi Punjabi Meals",
+      cloudinaryImageId: "bde8bfbd9092b770b4b6fb05232f51e1",
+      locality: "Aum Sai",
+      areaName: "Kharghar",
+      costForTwo: "₹250 for two",
+      cuisines: ["North Indian", "Punjabi", "Indian", "Home Food", "Beverages"],
+      avgRating: 3.7,
+      feeDetails: {
+        restaurantId: "432796",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 5300,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 5300,
+      },
+      parentId: "306004",
+      avgRatingString: "3.7",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 38,
+        lastMileTravel: 5,
+        serviceability: "SERVICEABLE",
+        slaString: "38 mins",
+        lastMileTravelString: "5.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-09-20 23:45:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹150 OFF",
+        subHeader: "ABOVE ₹399",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/paratha-box-desi-punjabi-meals-aum-sai-kharghar-mumbai-432796",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "26803",
+      name: "Cafe Coffee Day",
+      cloudinaryImageId: "b925459fbc1faf59f02f7289eb079a74",
+      locality: "Sector 2 Station Road",
+      areaName: "Kharghar",
+      costForTwo: "₹300 for two",
+      cuisines: [
+        "Beverages",
+        "Cafe",
+        "Snacks",
+        "Desserts",
+        "Burgers",
+        "Ice Cream",
+        "Bakery",
+        "Fast Food",
+      ],
+      avgRating: 4.1,
+      feeDetails: {
+        restaurantId: "26803",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 5300,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 5300,
+      },
+      parentId: "1",
+      avgRatingString: "4.1",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 37,
+        lastMileTravel: 5,
+        serviceability: "SERVICEABLE",
+        slaString: "37 mins",
+        lastMileTravelString: "5.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-09-20 23:45:00",
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: "guiltfree/GF_Logo_android_3x",
+            shortDescription: "options available",
+            fontColor: "#7E808C",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "",
+                  fontColor: "#7E808C",
+                  iconId: "guiltfree/GF_Logo_android_3x",
+                  shortDescription: "options available",
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹100",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/cafe-coffee-day-sector-2-station-road-kharghar-mumbai-26803",
+      type: "WEBLINK",
+    },
+  },
+];
+
+const ProductCard = ({
+  cloudinaryImageId,
+  name,
+  areaName,
+  costForTwo,
+  cuisines,
+  avgRating,
+}) => {
+  return (
+    <div className="card">
+      <img
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+        alt="product-card-img"
+      />
+      <div className="card-details">
+        <strong>{name}</strong>
+        <p>{areaName}</p>
+        <p style={{ color: "green" }}>{costForTwo}</p>
+        <p>{cuisines?.slice(0, 2).join(", ")}</p>
+        <p style={{color: "gold"}}>{avgRating}★</p>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <h2>Top restaurant chains in Mumbai</h2>
+      <div className="Products">
+        {restrauntList.map((restraunt) => (
+          <ProductCard {...restraunt.info} key={restraunt.info.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return <h1>Footer</h1>;
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
