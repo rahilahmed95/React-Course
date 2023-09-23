4969c23ec7c8fba10d301b0664fd9945fd2562c9
Rahil Ahmed <136297427+rahilahmed95@users.noreply.github.com>
2023-09-23 18:37:09 +0530

    Lecture 12 Uploaded

---
+++ Lecture 12/src/component/ShimmerDetails.js
@@ -0,0 +1,17 @@
+const ShimmerDetails = () => {
+  return (
+    <div className="flex flex-col justify-center items-center mt-14 h-screen">
+      <div className="w-80 h-52 bg-slate-900 opacity-30"></div>
+      <div className="w-80 h-72">
+        <p className="h-5 bg-gray-900 opacity-30 mt-3"></p>
+        <p className="h-5 bg-gray-900 opacity-30 mt-3"></p>
+        <p className="h-5 bg-gray-900 opacity-30 mt-3"></p>
+        <p className="h-5 bg-gray-900 opacity-30 mt-3"></p>
+        <p className="h-5 bg-gray-900 opacity-30 mt-3"></p>
+        <p className="h-5 bg-gray-900 opacity-30 mt-3"></p>
+      </div>
+    </div>
+  );
+};
+
+export default ShimmerDetails;
